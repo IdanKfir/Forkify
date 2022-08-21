251895b809dbe6b014b407f6262a4093336c70d6
IdanKfir <zunzer244@gmail.com>
2022-08-21 20:48:15 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '404a7bb1-894f-48c3-9fc4-b22c947b4394';
+export const MODAL_CLOSE_SEC = 2.5;
